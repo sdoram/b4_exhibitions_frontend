e67705f6a39062aeec3c5d25b3b79a01dd27675b
Seman_Kim <sdoramkim@gmail.com>
2023-06-07 15:27:19 +0900

    :sparkles:Feat: 내비바 기본 형태 작성 #38 #40
    내비바가 필요한 페이지에 header 태그 추가
    내비바의 기본적인 위치 지정
    
    [필요]
    a태그에서 button으로 변경
    로고 뒷배경 제거
    아이콘 추가 필요

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,1 +1,15 @@
 console.log('navbar 연결')
+
+async function injectNavbar() {
+    fetch("../templates/navbar.html").then(response => {
+        return response.text()
+    })
+        .then(data => {
+            document.querySelector("header").innerHTML = data;
+        });
+    const NavbarHTML = await fetch("../templates/navbar.html");
+    const data = await NavbarHTML.text();
+    document.querySelector("header").innerHTML = data;
+}
+
+injectNavbar();
