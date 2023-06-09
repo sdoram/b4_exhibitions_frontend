97f073e1a8a61e2346c9e49163112f4d16487532
Seman_Kim <sdoramkim@gmail.com>
2023-06-09 11:25:05 +0900

    :art:Style: 코드 컨벤션에 맞춰서 const 수정 #10

--- static/js/signin.js
+++ static/js/signin.js
@@ -14,13 +14,13 @@ async function handleSignIn() {
         })
     });
     // access,refresh 토큰 데이터 json으로 받기
-    const response_json = await response.json();
+    const responseJson = await response.json();
     // // 로컬 스토리지에 jwt 토큰 저장하기
-    localStorage.setItem("access", response_json.access);
-    localStorage.setItem("refresh", response_json.refresh);
-    console.log(response_json);
+    localStorage.setItem("access", responseJson.access);
+    localStorage.setItem("refresh", responseJson.refresh);
+    console.log(responseJson);
     // payload로 저장하기 
-    const base64Url = response_json.access.split('.')[1];
+    const base64Url = responseJson.access.split('.')[1];
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
@@ -28,7 +28,7 @@ async function handleSignIn() {
     localStorage.setItem("payload", jsonPayload);
     if (response.status == 200) {
         alert('로그인 성공');
-        window.location.replace(`${frontend_base_url}/`);
+        window.location.replace(`${frontendBaseUrl}/`);
     } else {
         alert(response.status);
     }
