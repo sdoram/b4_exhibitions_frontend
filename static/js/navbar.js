4a18d2d409f699bcac748ed4975f5641f7c14ace
Seman_Kim <sdoramkim@gmail.com>
2023-06-09 12:40:36 +0900

    :sparkles:Feat: 로그인 여부에 따라 navbar변경 #40
    localStorage에서 JWTToken 존재 여부에 따라 로그인 판단
    JWTToken 삭제로 로그아웃 함수 구현

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -10,6 +10,39 @@ async function injectNavbar() {
     const NavbarHTML = await fetch("../templates/navbar.html");
     const data = await NavbarHTML.text();
     document.querySelector("header").innerHTML = data;
+    if (payload) {
+        // 내비바 왼쪽 항목
+        const navbarLeft = document.getElementById("navbarLeft");
+
+        // 내비바 오른쪽 항목
+        const navbarRight = document.getElementById("navbarRight");
+        const logOutButton = document.createElement("button");
+        logOutButton.setAttribute("class", "text-warning ms-5 me-5 btn btn-light shadow-warning material-symbols-outlined");
+        logOutButton.innerText = "person 로그아웃";
+        logOutButton.setAttribute("onclick", "handleLogOut()");
+
+        const myPage = document.createElement("a");
+        myPage.setAttribute("class", "text-warning ms-5 me-5 btn btn-light shadow-warning material-symbols-outlined");
+        myPage.innerText = "person 내 정보";
+        myPage.setAttribute("href", `${frontendBaseUrl}/templates/my-page.html`);
+
+        navbarRight.appendChild(myPage);
+        navbarRight.appendChild(logOutButton);
+
+        // payload 존재시 숨길 항목
+        const signInButton = document.getElementById("signInButton");
+        const signUpButton = document.getElementById("signUpButton");
+        signInButton.style.display = "none";
+        signUpButton.style.display = "none";
+    }
+}
+
+// 로그아웃 함수 
+function handleLogOut() {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    localStorage.removeItem("payload");
+    location.reload();
 }
 
 injectNavbar();
