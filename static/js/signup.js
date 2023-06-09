ba5447bf37e024d6ea50089b6d167d6be79cad51
Seman_Kim <sdoramkim@gmail.com>
2023-06-09 11:12:20 +0900

    :sparkles:Feat: 일반 회원가입 기능 구현 #13

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,1 +1,30 @@
 console.log('signup 연결')
+
+async function handleSignUp() {
+    console.log('회원가입 버튼')
+    const email = document.getElementById("email").value;
+    const nickname = document.getElementById("nickname").value;
+    const password = document.getElementById("password").value;
+    const gender = document.getElementById("gender").value;
+    console.log(email, nickname, gender, password)
+
+    const response = await fetch(`${backendBaseUrl}/users/signup/`, {
+        headers: {
+            'content-type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            "email": email,
+            "nickname": nickname,
+            "password": password,
+            "gender": gender
+        })
+    });
+    console.log(response)
+    if (response.status == 201) {
+        alert("회원가입 성공")
+        window.location.replace(`${frontendBaseUrl}/templates/signin.html`)
+    } else {
+        alert(response.status)
+    }
+}
