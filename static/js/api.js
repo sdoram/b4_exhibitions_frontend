5f035802aafcbe687d7659f26b6ae8d9fbdf8630
Seman_Kim <sdoramkim@gmail.com>
2023-06-09 11:21:59 +0900

    :sparkles:Feat: Url과 JWTToken정보 저장 #4

--- static/js/api.js
+++ static/js/api.js
@@ -1,1 +1,15 @@
 console.log('api 연결')
+
+const frontendBaseUrl = "http://127.0.0.1:5500"
+const backendBaseUrl = "http://127.0.0.1:8000/api"
+
+const payload = localStorage.getItem("payload");
+const payloadParse = JSON.parse(payload);
+const token = localStorage.getItem("access");
+
+function checkSignIn() {
+    console.log('checkSignIn 연결 확인')
+    if (payload) {
+        window.location.replace(`${frontendBaseUrl}/`)
+    }
+}
