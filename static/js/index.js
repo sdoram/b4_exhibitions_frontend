61c86c87546ecc9b5451b1b09b6ec0bd4f480841
IamVine <donghyun.pv@gmail.com>
2023-06-15 19:49:09 +0900

    :art:Style:컨벤션 적용
    클래스 선택자의 이름을 케밥케이스로,
    js 파일 내 함수 이름을 카멜케이스로,
    변경하였습니다.

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,15 @@
 console.log('index 연결')
 
 // 좋아요 하트 관련 코드
-let full_heart = false;
+let fullHeart = false;
 function heart() {
     const heartElement = document.querySelector('#heart');
-    if (!full_heart) {
+    if (!fullHeart) {
     heartElement.style.backgroundImage = 'url("../static/img/filled-heart.png")';
     alert("좋아요!");
     } else {
     heartElement.style.backgroundImage = 'url("../static/img/empty-heart.png")';
     alert("좋아요 취소!");
     }
-    full_heart = !full_heart;
+    fullHeart = !fullHeart;
 }
