0e6a4598133b78481f4118e8ef24caa4c1a2d5de
Seman_Kim <sdoramkim@gmail.com>
2023-06-16 10:31:58 +0900

    :lipstick:Design: html에 맞춰 데이터 호출 #3
    [필요]
    이미지에 대한 수정
    전시 상세 연결
    예약하기 url 연결
    좋아요 데이터 연결

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,9 @@
 console.log('index 연결')
-import { googleAPI, frontendBaseURL, payload } from "./api.js";
+import { googleAPI, frontendBaseURL, payload, getExhibitionsAPI, backendBaseURL } from "./api.js";
 
 // 로컬 스토리지에 jwt 토큰 저장하기
 function setLocalStorage(responseJson) {
-	localStorage.setItem("access", responseJson.access);
+    localStorage.setItem("access", responseJson.access);
     localStorage.setItem("refresh", responseJson.refresh);
     const base64Url = responseJson.access.split('.')[1];
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
@@ -30,16 +30,104 @@ if (payload) {
             window.location.replace(`${frontendBaseURL}/templates/signin.html`);
         }
     })
-}    
+}
 
 // 좋아요 하트 관련 코드
 let fullHeart = false;
 function heart() {
     const heartElement = document.querySelector('#heart');
     if (!fullHeart) {
-    heartElement.style.backgroundImage = 'url("../static/img/filled-heart.png")';
+        heartElement.style.backgroundImage = 'url("../static/img/filled-heart.png")';
+        alert("좋아요!");
     } else {
-    heartElement.style.backgroundImage = 'url("../static/img/empty-heart.png")';
+        heartElement.style.backgroundImage = 'url("../static/img/empty-heart.png")';
+        alert("좋아요 취소!");
     }
     fullHeart = !fullHeart;
 }
+
+
+// function exhibitionDetail(exhibition_id) {
+//     console.log('전시회 디테일', exhibition_id)
+// }
+
+window.onload = function loadExhibitions() {
+    getExhibitionsAPI().then(({ response, responseJson }) => {
+        const exhibitionsDATA = responseJson.results
+        console.log(exhibitionsDATA)
+        const exhibitionList = document.getElementById("exhibitionList")
+        exhibitionsDATA.forEach(exhibition => {
+            const exhibitionSet = document.createElement("div");
+            exhibitionSet.setAttribute("class", "exhibition-set");
+
+
+            const exhibitionImgBox = document.createElement("div");
+            exhibitionImgBox.setAttribute("class", "exhibition-img-box")
+            exhibitionSet.appendChild(exhibitionImgBox)
+
+            // 전시회 이미지
+            const exhibitionImg = document.createElement("img");
+            // 이미지 사이즈가 클 경우 화면에 맞게 줄여주는 css 수정 필요
+            exhibitionImg.setAttribute("class", "card-img-top");
+            // 이미지를 못찾을 경우 대체 이미지 
+            exhibitionImg.setAttribute("onerror", "this.src='static/img/default-img.jpg'")
+            if (exhibition.image) {
+                // 대체 url 코드로 인코딩된 url 디코딩 하기
+                exhibitionImg.setAttribute("src", decodeURIComponent(exhibition.image.split("media/")[1]));
+            } else {
+                exhibitionImg.setAttribute("src", "static/img/default-img.jpg")
+            }
+            exhibitionImgBox.appendChild(exhibitionImg)
+
+            // 전시회 정보 박스 
+            const exhibitionInfoBox = document.createElement("div");
+            exhibitionInfoBox.setAttribute("class", "exhibition-info-box");
+            exhibitionSet.appendChild(exhibitionInfoBox)
+
+            // 전시회 제목
+            const exhibitionTitle = document.createElement("span");
+            exhibitionTitle.setAttribute("class", "exhibition-title");
+            exhibitionTitle.innerText = exhibition.info_name
+            exhibitionInfoBox.appendChild(exhibitionTitle);
+
+            // 전시회 기간
+            const exhibitionPeriod = document.createElement("span");
+            exhibitionPeriod.setAttribute("class", "exhibition-period")
+            exhibitionPeriod.innerText = `${exhibition.start_date} ~ ${exhibition.end_date}`
+            exhibitionInfoBox.appendChild(exhibitionPeriod)
+
+            // 전시회 좋아요 
+            const exhibitionHeartSet = document.createElement("div")
+            exhibitionHeartSet.setAttribute("class", "heart-set")
+            exhibitionInfoBox.appendChild(exhibitionHeartSet)
+
+            const exhibitionHeart = document.createElement("div")
+            exhibitionHeart.setAttribute("class", "heart")
+            exhibitionHeartSet.appendChild(exhibitionHeart)
+
+            const exhibitionHeartNum = document.createElement("span")
+            exhibitionHeartNum.setAttribute("class", "heart-num")
+            // 백엔드 정보로 수정 필요 
+            exhibitionHeartNum.innerText = '1'
+            exhibitionHeartSet.appendChild(exhibitionHeartNum)
+
+            // 상세 & 예약 박스
+            const exhibitionSignSet = document.createElement('div')
+            exhibitionSignSet.setAttribute("class", "sign-set")
+            exhibitionInfoBox.appendChild(exhibitionSignSet)
+
+            const exhibitionDetailButton = document.createElement("button")
+            exhibitionDetailButton.setAttribute("class", "detail-button")
+            exhibitionDetailButton.innerText = '전시상세'
+            exhibitionSignSet.appendChild(exhibitionDetailButton)
+
+            const exhibitionReserveButton = document.createElement("button")
+            exhibitionReserveButton.setAttribute("class", "reserve-button")
+            exhibitionReserveButton.innerText = '예약하기'
+            exhibitionSignSet.appendChild(exhibitionReserveButton)
+
+            exhibitionList.appendChild(exhibitionSet)
+        })
+    })
+}
+
