893bdf8cba4fc4e96eba72635783315abd091a36
Seman_Kim <sdoramkim@gmail.com>
2023-06-21 16:00:03 +0900

    :sparkles:Feat: 전시회 작성 구현 #25
    현재 작성한 게시글로 바로 이동시 데이터 조회 불가
    backend 서버 recommnd에서 데이터 바로 반영 불가
    서버 재 실행시 조회 가능

--- static/js/exhibition-posting.js
+++ static/js/exhibition-posting.js
@@ -1,1 +1,42 @@
 console.log('exhibition-posting 연결')
+
+import { exhibitionPostingAPI, frontendBaseURL } from "./api.js";
+
+function handleExhibitionPosting() {
+    const exhibitionCategory = document.getElementById("exhibitionCategory").value;
+    const exhibitionTitle = document.getElementById('exhibitionTitle').value;
+    const exhibitionLocation = document.getElementById('exhibitionLocation').value;
+    const exhibitionStartDate = document.getElementById('exhibitionStartDate').value;
+    const exhibitionEndDate = document.getElementById('exhibitionEndDate').value;
+    const exhibitionImage = document.getElementById('exhibitionImage').files[0];
+    const exhibitionContent = document.getElementById('exhibitionContent').value;
+    const exhibitionService = document.getElementById('exhibitionService').value;
+    const exhibitionURL = document.getElementById('exhibitionURL').value;
+
+    // API 전달용 data
+    const data = new FormData();
+    data.append("category", exhibitionCategory)
+    data.append("info_name", exhibitionTitle)
+    data.append("location", exhibitionLocation)
+    data.append("start_date", exhibitionStartDate)
+    data.append("end_date", exhibitionEndDate)
+    data.append("image", exhibitionImage || '')
+    data.append("content", exhibitionContent)
+    data.append("svstatus", exhibitionService)
+    data.append("direct_url", exhibitionURL)
+
+
+    exhibitionPostingAPI(data).then(({ response, responseJson }) => {
+        if (response.status == 201) {
+            console.log(responseJson.data)
+            alert(responseJson.message);
+            window.location.replace(`${frontendBaseURL}/templates/exhibition-detail.html?exhibition_id=${responseJson.data.id}`)
+        } else {
+            alert(responseJson.message);
+            console.log(response.status);
+        }
+    });
+
+}
+
+document.getElementById("exhibitionPostingButton").addEventListener("click", handleExhibitionPosting);
