a3a17f62de8ea8fbd3b29820e3cd0f9736e873d4
IamVine <donghyun.pv@gmail.com>
2023-06-22 16:27:15 +0900

    :art:Style:각종 화면구현 오류 수정
    1. 리뷰보기 클릭 후 전시 상세페이지에서 카드들이 삐뚤빼뚤 못생기게
       보이는 현상을 수정했습니다.
    
    2. 리뷰카드나 동행글 카드가 달리기 시작하면 화면 줄임을 했을 때 우측
       추천전시 목록이 다가와서 서로 시야를 가리는 문제가 있었습니다.
       때문에 리뷰버튼이나 동행글 버튼이 눌리면 추천전시 목록이 화면에서
       사라지도록 했습니다.
       연관해서 동행글 보기의 id를 새로 지어주었습니다. accompanyBtn
       입니다. 동행글 담당자와 상의한 후 지었으므로 괜찮습니다.

--- static/js/exhibition-detail.js
+++ static/js/exhibition-detail.js
@@ -107,6 +107,18 @@ function exhibitionReserve(link) {
     window.open(link)
 }
 
+// 리뷰 버튼이나 동행글 버튼을 누르면 우측 전시추천 항목이 사라진다.
+document.getElementById("reviewBtn").addEventListener("click", function() {
+    hideRecommendOrganizer(); // 클릭 이벤트 발생시 숨기기 처리
+});
+document.getElementById("accompanyBtn").addEventListener("click", function() {
+    hideRecommendOrganizer(); // 동행글 보기 버튼 클릭 이벤트 처리
+});
+function hideRecommendOrganizer() {
+    var recommendOrganizer = document.querySelector(".recommend-organizer");
+    recommendOrganizer.style.display = "none";
+}
+
 // 리뷰 버튼 눌렀을 때 실행되는 함수
 function review(exhibition_id) {
     reviewGetAPI(exhibition_id).then(({ responseJson }) => {
@@ -115,18 +127,18 @@ function review(exhibition_id) {
 
         const exhibitionDetail = document.getElementById("exhibitionDetail")
         const reviewList = document.createElement("div")
-        reviewList.setAttribute("class", "all-items-organizer")
+        reviewList.setAttribute("class", "rv-all-items-organizer")
 
         // 리뷰 목록
         reviewsDATA.forEach(review => {          
             const grayBox = document.createElement("div")
-            grayBox.setAttribute("class", "gray-box")
+            grayBox.setAttribute("class", "rv-gray-box")
 
             // 이미지
             const imgBox = document.createElement("div")
-            imgBox.setAttribute("class", "img-box")
+            imgBox.setAttribute("class", "rv-img-box")
             const reviewImg = document.createElement("img")
-            reviewImg.setAttribute("class", "review-img")
+            reviewImg.setAttribute("class", "rv-review-img")
             reviewImg.setAttribute("onerror", "this.src='/static/img/default-img.jpg'")
             if (review.image) {                
                 reviewImg.setAttribute("src", `${backendBaseURL.split('/api')[0]}${review.image}`);             
@@ -138,30 +150,30 @@ function review(exhibition_id) {
 
             
             const purpleBox = document.createElement("div")
-            purpleBox.setAttribute("class", "purple-box")
+            purpleBox.setAttribute("class", "rv-purple-box")
 
             const row1InPurple = document.createElement("div")
-            row1InPurple.setAttribute("class", "row1-in-purple")
+            row1InPurple.setAttribute("class", "rv-row1-in-purple")
 
             // 닉네임
             const nicknameBox = document.createElement("div")
-            nicknameBox.setAttribute("class", "nickname-box")
+            nicknameBox.setAttribute("class", "rv-nickname-box")
             nicknameBox.innerText = review.nickname
             row1InPurple.appendChild(nicknameBox)
 
             // 별점
             const stars = document.createElement("div")
-            stars.setAttribute("class", "stars")
+            stars.setAttribute("class", "rv-stars")
             
             for (let i = 1; i <= 5; i++) {
                 if (i <= review.rating) {  
                     let star = document.createElement("img")
-                    star.setAttribute("class", "star")              
+                    star.setAttribute("class", "rv-star")              
                     star.setAttribute("src", "/static/img/filled-star.png")
                     stars.appendChild(star)
                 } else {
                     let star = document.createElement("img")
-                    star.setAttribute("class", "star") 
+                    star.setAttribute("class", "rv-star") 
                     star.setAttribute("src", "/static/img/empty-star.png")
                     stars.appendChild(star)
                 }
@@ -170,21 +182,21 @@ function review(exhibition_id) {
             purpleBox.appendChild(row1InPurple)
 
             const row2InPurple = document.createElement("div")
-            row2InPurple.setAttribute("class", "row2-in-purple")
+            row2InPurple.setAttribute("class", "rv-row2-in-purple")
 
             // 리뷰 내용
             const reviewContent = document.createElement("div")
-            reviewContent.setAttribute("class", "review-content")
+            reviewContent.setAttribute("class", "rv-review-content")
             reviewContent.innerText = review.content
             row2InPurple.appendChild(reviewContent)
             purpleBox.appendChild(row2InPurple)
 
             const row3InPurple = document.createElement("div")
-            row3InPurple.setAttribute("class", "row3-in-purple")
+            row3InPurple.setAttribute("class", "rv-row3-in-purple")
 
             // 리뷰 날짜
             const dateInfo = document.createElement("div")
-            dateInfo.setAttribute("class", "date-info")
+            dateInfo.setAttribute("class", "rv-date-info")
             const span1 = document.createElement("span")
             span1.innerText = "리뷰 최종 수정일"
             dateInfo.appendChild(span1)
@@ -199,14 +211,14 @@ function review(exhibition_id) {
                     // 수정 버튼
                     const reviewUpdateBtn = document.createElement("button")
                     reviewUpdateBtn.setAttribute("type", "button")
-                    reviewUpdateBtn.setAttribute("class", "review-update-btn")
+                    reviewUpdateBtn.setAttribute("class", "rv-review-update-btn")
                     reviewUpdateBtn.innerText = "수정"
                     row3InPurple.appendChild(reviewUpdateBtn)
 
                     // 삭제 버튼
                     const reviewDeleteBtn = document.createElement("button")
                     reviewDeleteBtn.setAttribute("type", "button")
-                    reviewDeleteBtn.setAttribute("class", "review-delete-btn")
+                    reviewDeleteBtn.setAttribute("class", "rv-review-delete-btn")
                     reviewDeleteBtn.innerText = "삭제"
                     row3InPurple.appendChild(reviewDeleteBtn)
                 }
