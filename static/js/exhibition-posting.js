81d3d9b9660e6f87a6ff11a1bcaa77c5b3a63901
Seman_Kim <sdoramkim@gmail.com>
2023-06-22 12:29:14 +0900

    :sparkles:Feat: 전시회 수정 추가 #25
    `function exhibitionInPutInfo()`
    html에 입력된 데이터를 가져오는 함수 선언
    
    ```javascript
    function exhibitionInPutInfo()
    '''
    return data
    
    function handleExhibitionPosting() {
        exhibitionPostingAPI(exhibitionInPutInfo()).then(({ response, responseJson })
    ```
    API함수에 Info함수의 return값 집어넣기
    
    `const URLParams = new URL(location.href).searchParams;`
    ?exhibition_id가 존재하는지 판단하기위한 준비

--- static/js/exhibition-posting.js
+++ static/js/exhibition-posting.js
@@ -1,8 +1,9 @@
 console.log('exhibition-posting 연결')
 
-import { exhibitionPostingAPI, frontendBaseURL } from "./api.js";
+import { exhibitionPostingAPI, frontendBaseURL, exhibitionPutAPI, getExhibitionAPI } from "./api.js";
 
-function handleExhibitionPosting() {
+// 게시글 작성 정보 가져오기
+function exhibitionInPutInfo() {
     const exhibitionCategory = document.getElementById("exhibitionCategory").value;
     const exhibitionTitle = document.getElementById('exhibitionTitle').value;
     const exhibitionLocation = document.getElementById('exhibitionLocation').value;
@@ -20,13 +21,18 @@ function handleExhibitionPosting() {
     data.append("location", exhibitionLocation)
     data.append("start_date", exhibitionStartDate)
     data.append("end_date", exhibitionEndDate)
-    data.append("image", exhibitionImage || '')
+    if (exhibitionImage) {
+        data.append("image", exhibitionImage)
+    }
     data.append("content", exhibitionContent)
     data.append("svstatus", exhibitionService)
     data.append("direct_url", exhibitionURL)
 
+    return data
+}
 
-    exhibitionPostingAPI(data).then(({ response, responseJson }) => {
+function handleExhibitionPosting() {
+    exhibitionPostingAPI(exhibitionInPutInfo()).then(({ response, responseJson }) => {
         if (response.status == 201) {
             console.log(responseJson.data)
             alert(responseJson.message);
@@ -39,4 +45,49 @@ function handleExhibitionPosting() {
 
 }
 
-document.getElementById("exhibitionPostingButton").addEventListener("click", handleExhibitionPosting);
+function handleExhibitionPut(exhibition_id) {
+    exhibitionPutAPI(exhibition_id, exhibitionInPutInfo()).then(({ response, responseJson }) => {
+        if (response.status == 200) {
+            alert('수정했습니다');
+            window.location.replace(`${frontendBaseURL}/templates/exhibition-detail.html?exhibition_id=${exhibition_id}`)
+        } else {
+            alert('수정 실패')
+        }
+    });
+}
+
+
+// 수정하기인지 작성하기인지 판단하기
+const URLParams = new URL(location.href).searchParams;
+if (URLParams.get('exhibition_id')) {
+    window.onload = function () {
+        const exhibition_id = new URLSearchParams(window.location.search).get("exhibition_id")
+
+        getExhibitionAPI(exhibition_id).then(({ response, responseJson }) => {
+            const exhibitionInfo = responseJson
+            console.log(exhibitionInfo)
+            document.getElementById("exhibitionCategory").value = exhibitionInfo.category
+            document.getElementById('exhibitionTitle').value = exhibitionInfo.info_name
+            document.getElementById('exhibitionLocation').value = exhibitionInfo.location
+            document.getElementById('exhibitionStartDate').value = exhibitionInfo.start_date
+            document.getElementById('exhibitionEndDate').value = exhibitionInfo.end_date
+            document.getElementById('exhibitionService').value = exhibitionInfo.svstatus
+            document.getElementById('exhibitionURL').value = exhibitionInfo.direct_url
+            document.getElementById('exhibitionContent').value = exhibitionInfo.content
+            // 이미지 문제 해결 필요 
+            // document.getElementById('exhibitionImage').files[0] = exhibitionInfo.image
+            // const exhibitionImage = document.getElementById('exhibitionImage')
+        })
+    }
+    document.getElementById("exhibitionPutButton").addEventListener("click", function () {
+        console.log(URLParams.get('exhibition_id'))
+        handleExhibitionPut(URLParams.get('exhibition_id'))
+    }
+    );
+    // 전시회 수정, 등록 전환 
+    document.getElementById("exhibitionPostingButton").style.display = "none";
+    document.getElementById("welcomeTitle").innerText = '전시회 수정하기'
+} else {
+    document.getElementById("exhibitionPostingButton").addEventListener("click", handleExhibitionPosting);
+    document.getElementById("exhibitionPutButton").style.display = "none";
+}
