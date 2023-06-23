5e8be3f4a620363579a5e70e9efb073f5babb589
Seman_Kim <sdoramkim@gmail.com>
2023-06-23 11:29:50 +0900

    :art:Style: API 코드 위치 조정 #4
    유저 관련 API함수 위치를 이어서 볼 수 있도록 이동

--- static/js/api.js
+++ static/js/api.js
@@ -51,9 +51,43 @@ export async function googleAPI(google_token) {
     }
 }
 
+// 회원 정보 불러오기 API
+export async function getUserInfoAPI(user_id) {
+    const response = await fetch(`${backendBaseURL}/users/${user_id}`)
+    const responseJson = await response.json();
+    console.log(response, responseJson);
+    return { response, responseJson };
+}
+
+// 회원 정보 수정 API
+export async function patchUserInfoAPI(data) {
+    const response = await fetch(`${backendBaseURL}/users/`, {
+        method: "PATCH",
+        headers: { "Authorization": `Bearer ${token}` },
+        body: data
+    })
+    const responseJson = await response.json();
+    console.log(response, responseJson)
+    return { response, responseJson }
+}
+
+// 회원 탈퇴 API
+export async function deleteUserInfoAPI(user_id) {
+    if (user_id == payloadParse.user_id) {
+        const response = await fetch(`${backendBaseURL}/users/`, {
+            method: "DELETE",
+            headers: { "Authorization": `Bearer ${token}` }
+        })
+        const responseJson = await response.json();
+        console.log(response, responseJson)
+        return { response, responseJson }
+    } else {
+        return alert('본인이 아닙니다')
+    }
+}
+
 // 메인 페이지 전시회 전체 조회 API
 export async function getExhibitionsAPI() {
-
     const URLParams = new URL(location.href).searchParams;
     const page = URLParams.get('page')
     const category = URLParams.get('category')
@@ -61,7 +95,6 @@ export async function getExhibitionsAPI() {
     if (page != null) {
         // 페이지 정보가 있는 경우
         if (category) {
-
             const response = await fetch(`${backendBaseURL}/exhibitions/?category=${category}&page=${page}`)
             const responseJson = await response.json();
             return { response, responseJson };
@@ -91,6 +124,7 @@ export async function getExhibitionsAPI() {
         }
     }
 }
+
 // 전시회 상세 페이지 API
 export async function getExhibitionAPI(exhibition_id) {
     const response = await fetch(`${backendBaseURL}/exhibitions/${exhibition_id}`)
@@ -108,41 +142,6 @@ export async function postExhibitionLikeAPI(exhibition_id) {
     console.log(response, responseJson)
 
     return { response, responseJson };
-}
-
-// 마이 페이지 API
-export async function getUserInfoAPI(user_id) {
-    const response = await fetch(`${backendBaseURL}/users/${user_id}`)
-    const responseJson = await response.json();
-    console.log(response, responseJson);
-    return { response, responseJson };
-}
-
-// 회원 탈퇴 API
-export async function deleteUserInfoAPI(user_id) {
-    if (user_id == payloadParse.user_id) {
-        const response = await fetch(`${backendBaseURL}/users/`, {
-            method: "DELETE",
-            headers: { "Authorization": `Bearer ${token}` }
-        })
-        const responseJson = await response.json();
-        console.log(response, responseJson)
-        return { response, responseJson }
-    } else {
-        return alert('본인이 아닙니다')
-    }
-}
-
-// 유저 정보 수정
-export async function patchUserInfoAPI(data) {
-    const response = await fetch(`${backendBaseURL}/users/`, {
-        method: "PATCH",
-        headers: { "Authorization": `Bearer ${token}` },
-        body: data
-    })
-    const responseJson = await response.json();
-    console.log(response, responseJson)
-    return { response, responseJson }
 }
 
 // 전시회 작성
