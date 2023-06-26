027d0d1010c1d6e0af2635af8f2d2203e61ac327
minjeong <ehdro418@naver.com>
2023-06-26 19:11:21 +0900

    :sparkles:Feat: 리뷰/동행글 수정 상태에서 예약하기 클릭 시 경고창 띄우기 #22
    1. isdEditingReview, isEdtingAccompany 변수를 import 하여 둘 중 하나라도 값이 true 인 경우 예약하기 버튼 클릭했을 때 수정 상태를 마무리 하라는 경고창이 뜨도록 했습니다.
    2. import 부분을 제외하고 세미콜론을 모두 삭제했습니다.

--- static/js/exhibition-detail.js
+++ static/js/exhibition-detail.js
@@ -2,8 +2,8 @@ console.log('exhibition-detail 연결')
 
 
 import { getExhibitionAPI, postExhibitionLikeAPI, payload, payloadParse, getUserInfoAPI, frontendBaseURL } from "./api.js";
-import { review } from "./review.js";
-import { accompany } from "./accompany.js";
+import { review, isEditingReview } from "./review.js";
+import { accompany, isEditingAccompany } from "./accompany.js";
 
 
 window.onload = function loadExhibition() {
@@ -14,18 +14,18 @@ window.onload = function loadExhibition() {
         console.log(exhibitionDATA)
 
         // 전시회 이미지
-        const exhibitionImg = document.getElementById("posterImg");
+        const exhibitionImg = document.getElementById("posterImg")
         if (exhibitionDATA.image) {
             if (exhibitionDATA.image.includes('https:')) {
-                exhibitionImg.setAttribute("src", exhibitionDATA.image);
+                exhibitionImg.setAttribute("src", exhibitionDATA.image)
             } else {
                 // 대체 url 코드로 인코딩된 url 디코딩 하기    
-                exhibitionImg.setAttribute("src", decodeURIComponent(exhibitionDATA.image.split("media/")[1]));
+                exhibitionImg.setAttribute("src", decodeURIComponent(exhibitionDATA.image.split("media/")[1]))
             }
         }
 
         // // 전시회 좋아요 
-        const exhibitionHeart = document.getElementById("heart");
+        const exhibitionHeart = document.getElementById("heart")
         exhibitionHeart.addEventListener("click", function () {
             heart(exhibitionDATA.id)
         })
@@ -39,31 +39,31 @@ window.onload = function loadExhibition() {
             getUserInfoAPI(payloadParse.user_id).then(({ responseJson }) => {
                 responseJson.exhibition_likes.forEach((obj) => {
                     if (exhibitionDATA.id == obj.id) {
-                        const heartElement = document.getElementById("heart");
-                        heartElement.style.backgroundImage = 'url("../static/img/filled-heart.png")';
+                        const heartElement = document.getElementById("heart")
+                        heartElement.style.backgroundImage = 'url("../static/img/filled-heart.png")'
                     }
                 })
             })
         }
 
         // 전시회 제목
-        const exhibitionTitle = document.getElementById("title");
+        const exhibitionTitle = document.getElementById("title")
         exhibitionTitle.innerHTML = exhibitionDATA.info_name
 
         // 전시회 장소
-        const exhibitionLocation = document.getElementById("location");
+        const exhibitionLocation = document.getElementById("location")
         exhibitionLocation.innerHTML = exhibitionDATA.location
 
         // 전시회 시작일
-        const exhibitionStartDate = document.getElementById("startDate");
+        const exhibitionStartDate = document.getElementById("startDate")
         exhibitionStartDate.innerText = exhibitionDATA.start_date
 
         // 전시회 종료일
-        const exhibitionEndDate = document.getElementById("endDate");
+        const exhibitionEndDate = document.getElementById("endDate")
         exhibitionEndDate.innerText = exhibitionDATA.end_date
 
         // 전시회 설명
-        const exhibitionContent = document.getElementById("content");
+        const exhibitionContent = document.getElementById("content")
         exhibitionContent.innerHTML = exhibitionDATA.content
 
         // 전시 추천바
@@ -78,10 +78,10 @@ window.onload = function loadExhibition() {
             let recommendImg = document.getElementById(`${i}-rec-img`)
             if (recommend.image) {
                 if (recommend.image.includes('https:')) {
-                    recommendImg.setAttribute("src", recommend.image);
+                    recommendImg.setAttribute("src", recommend.image)
                 } else {
                     // 대체 url 코드로 인코딩된 url 디코딩 하기    
-                    recommendImg.setAttribute("src", decodeURIComponent(recommend.image.split("media/")[1]));
+                    recommendImg.setAttribute("src", decodeURIComponent(recommend.image.split("media/")[1]))
                 }
             }
 
@@ -91,40 +91,45 @@ window.onload = function loadExhibition() {
         }
 
         // 리뷰 버튼
-        const reviewButton = document.getElementById("reviewBtn");
+        const reviewButton = document.getElementById("reviewBtn")
         reviewButton.addEventListener("click", function () {
-            review(exhibition_id);
-        });
+            review(exhibition_id)
+        })
 
         // 동행 버튼
-        const accompanyButton = document.getElementById("accompanyBtn");
+        const accompanyButton = document.getElementById("accompanyBtn")
         accompanyButton.addEventListener("click", function () {
-            accompany(exhibition_id);
-        });
+            accompany(exhibition_id)
+        })
 
         // 예약하기 버튼
-        const exhibitionReserveButton = document.getElementById("reserveBtn");
+        const exhibitionReserveButton = document.getElementById("reserveBtn")
         exhibitionReserveButton.addEventListener("click", function () {
-            exhibitionReserve(exhibitionDATA.direct_url);
-        });
+            if (isEditingAccompany || isEditingReview) {
+                alert("수정하고 있는 글을 저장 또는 취소 후 클릭하십시오.")
+            } else {
+                exhibitionReserve(exhibitionDATA.direct_url)
+            }
+            
+        })
     })
 
 }
 
 // 좋아요 하트 관련 코드
 function heart(exhibition_id) {
-    let fullHeart = false;
+    let fullHeart = false
     postExhibitionLikeAPI(exhibition_id).then(({ response, responseJson }) => {
-        const heartElement = document.getElementById("heart");
+        const heartElement = document.getElementById("heart")
         const heartNum = document.getElementById("heartNum")
         if (response.status == 201) {
-            heartElement.style.backgroundImage = 'url("/static/img/filled-heart.png")';
+            heartElement.style.backgroundImage = 'url("/static/img/filled-heart.png")'
             heartNum.innerText = responseJson.likes
         } else {
-            heartElement.style.backgroundImage = 'url("/static/img/empty-heart.png")';
+            heartElement.style.backgroundImage = 'url("/static/img/empty-heart.png")'
             heartNum.innerText = responseJson.likes
         }
-        fullHeart = !fullHeart;
+        fullHeart = !fullHeart
     })
 }
 
@@ -134,18 +139,18 @@ function exhibitionReserve(link) {
 }
 
 // 스크롤 위치에 따른 추천바 숨기기
-let recommendOrganizer = document.querySelector(".recommend-organizer");
-let reserveBtn = document.querySelector("#reserveBtn");
-let reserveBtnHeight = window.pageYOffset + reserveBtn.getBoundingClientRect().top;
+let recommendOrganizer = document.querySelector(".recommend-organizer")
+let reserveBtn = document.querySelector("#reserveBtn")
+let reserveBtnHeight = window.pageYOffset + reserveBtn.getBoundingClientRect().top
 
-let header = document.querySelector("header");
-let headerHeight = window.pageYOffset + header.getBoundingClientRect().top;
+let header = document.querySelector("header")
+let headerHeight = window.pageYOffset + header.getBoundingClientRect().top
 
 window.onscroll = function () {
-    let windowTop = window.scrollY;
+    let windowTop = window.scrollY
     if (windowTop >= reserveBtnHeight || windowTop <= headerHeight) {
-        recommendOrganizer.style.display = "none";
+        recommendOrganizer.style.display = "none"
     } else {
-        recommendOrganizer.style.display = "flex";
+        recommendOrganizer.style.display = "flex"
     }
 }
