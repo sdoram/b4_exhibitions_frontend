30566d8f87b4ba994af23e3059157dcddc1a076c
minjeong <ehdro418@naver.com>
2023-06-28 15:54:32 +0900

    :sparkles:Feat: frontend base url 변경

--- static/js/api.js
+++ static/js/api.js
@@ -1,7 +1,7 @@
 console.log('api 연결')
 
 // 배포용 URL
-export const frontendBaseURL = "https://b4-exhibitions-front.netlify.app";
+export const frontendBaseURL = "https://b4-exhibitions-now.netlify.app";
 export const backendBaseURL = "https://www.b4-exhibitions.shop/api";
 
 // 로컬용 URL
