b5da5362ab663f6e8a5dd052019fbb3f0592da9c
banghyunjae <banghyunjae@naver.com>
2023-06-29 18:02:53 +0900

    :sparkles:Feat: 날씨 API를 받아오는 함수 이동
    weather.js 파일을 삭제하고 날씨API를 받아오는 함수를
    navbar.js로 이동했습니다.

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,5 +1,5 @@
-import { frontendBaseURL, payload, payloadParse } from "./api.js";
-
+import { frontendBaseURL, payload, payloadParse, backendBaseURL } from "./api.js";
+const API_URL = `${backendBaseURL}/exhibitions/weather/`
 async function injectNavbar() {
     fetch("../templates/navbar.html").then(response => {
         return response.text()
@@ -16,6 +16,44 @@ async function injectNavbar() {
         document.getElementById(i).addEventListener("click", function () {
             selectCategory(this.value)
         })
+    updateWeather();
+
+    async function fetchWeatherData() {
+        try {
+            const response = await fetch(API_URL);
+
+            if (response.ok) {
+                const data = await response.json();
+                const iconUrl = getWeatherIconUrl(data.weather[0].icon);
+                return { data, iconUrl };
+            } else {
+                throw new Error(`Failed to fetch weather data: ${response.status}`);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    // 날씨 아이콘  URL
+    function getWeatherIconUrl(iconId) {
+        return `https://openweathermap.org/img/wn/${iconId}.png`;
+    }
+    // 날씨 정보 업데이트
+    function updateWeather() {
+        document.getElementById("weather-text").innerHTML = "로딩 중...";
+
+        fetchWeatherData()
+            .then(({ data, iconUrl }) => {
+                const { main, weather } = data;
+                const img = document.createElement("img");
+                const weatherText = `${Math.round(main.temp * 10) / 10}ºC`;
+                img.src = iconUrl;
+                img.alt = weather[0].description;
+                const weatherElement = document.getElementById("weather-info");
+                weatherElement.innerHTML = weatherText;
+                weatherElement.appendChild(img);
+            })
+    }
+
 
     if (payload) {
         // 내비바 왼쪽 항목
